2d7b9ca3055e237ea441fcee555195fb0259e6e1
JustinYates <Justin.yates24601@gmail.com>
2018-02-01 16:36:19 -0500

    Early stage mana curve generator

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("type your card mana type to start");
+
+        Scanner ManaType = new Scanner(System.in);
+
+        Scanner ManaCost = new Scanner(System.in);
+
+        Scanner CardName = new Scanner(System.in);
+
+        Scanner Colorless = new Scanner(System.in);
+
+        String SmanaCost = ManaType.nextLine();
+
+        System.out.println("How much " + SmanaCost + " mana " + "is the card?: ");
+
+        int manaCost = ManaCost.nextInt();
+
+        System.out.print("Name of the card?: ");
+
+        String Cname = CardName.nextLine();
+
+        System.out.println(" And how much colorless mana if any: ?");
+
+        int ColorlessC = Colorless.nextInt();
+
+        System.out.print("So your " + Cname + " card " + "cost" + " " + manaCost + " " + SmanaCost + " mana "  + "with " + ColorlessC + " " + "colorless mana?");
+
+
+    }
+}
